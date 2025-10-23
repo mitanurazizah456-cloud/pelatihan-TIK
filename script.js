94d49fed388cd0c6ec713d7ee59432636752a9ce
mitanurazizah456-cloud <mitanurazizah456@gmail.com>
2025-10-23 10:11:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasarkan ID-nya
+const tombolsapa = document.getElementById('sapaButton');
+//2.tambahkan 'event listener' untuk aksi click'
+tombolsapa.addEvent('click',function() {
+  // 3. jalankan kode ini ketika tombol di-klik
+                    alert('halo! terima kasih sudah berkunjung!');
+});
